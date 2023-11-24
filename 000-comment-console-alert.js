79d3c43f68c1cbbd20e028301075ee89d6ec5394
AbidStack <abidstack@gmail.com>
2023-11-24 23:07:27 +0600

    added a commented line for testing purpose

--- 000-comment-console-alert.js
+++ 000-comment-console-alert.js
@@ -1,9 +1,11 @@
+
+
 console.log(`I Love Programming...!!!`);
 console.log(`I Love the color of money...!!!`);
 
 Window.alert(`Do you love programming and the color of money like me...???`);
 
-//This is a single line comment..//
+//This is a single line comment...//
 
 /*
 
@@ -14,3 +16,5 @@ Window.alert(`Do you love programming and the color of money like me...???`);
   comment...
 
 */
+
+//This are the two types of comment available in javaScript
