8997ead10359f46a9943d1a98359872af9b61f6b
AbidStack <abidstack@gmail.com>
2023-11-24 23:13:02 +0600

    comment added

--- 019-break-continue.js
+++ 019-break-continue.js
@@ -1,8 +1,9 @@
 for (i = 0; i <= 20; i++) {
     if (i == 13) {
         continue;
+        console.log(`continue will skip if condition matched...`);
     }
-    if (i == 15) {
+    if (i == 19) {
         break;        
     }
     console.log(i)
